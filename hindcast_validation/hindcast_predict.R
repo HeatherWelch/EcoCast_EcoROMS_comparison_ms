7ae56d3a1adfe710a39f6b2bf219f786fd16745d
HeatherWelch <heather.welch@noaa.gov>
2018-01-29 17:18:15 -0800

    more stuff

--- hindcast_validation/hindcast_predict.R
+++ hindcast_validation/hindcast_predict.R
@@ -1,3 +1,66 @@
 #### code to predict the EcoCast and EcoROMS species models at the locations/times of catch/bycatch events
-### 3. predict for each species at each raw data point (different script)
-### 4. create Eco products under conditions of different species weightings (different script)
+### 3. predict for each species at each raw data point
+### 4. create Eco products under conditions of different species weightings
+
+source("/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/load_functions.R")
+
+## A. rasterRescale
+Rescale<-function(r){
+  r.min = min(r)
+  r.max = max(r)
+  r.scale <- ((r - r.min) / (r.max - r.min) - 0.5 ) * 2
+  return(r.scale) #(r-rmin)/(rmax-rmin)
+}
+
+## A2. rasterRescale (-1 to r.max) ## this is for when swordfish = 0, we still rescale the min value to -1 to fit within app.R color range ##test
+alt_Rescale=function(r){
+  r.min = min(r)
+  r.max = max(r)
+  r.scale = -1+(0--1)*(r-r.min)/(r.max-r.min)
+  return(r.scale)
+}
+
+### 3. predict for each species at each raw data point ####
+## 1. read in species models
+swor=readRDS("/Users/heatherwelch/Dropbox/Eco-ROMS/Model Outputs/Final EcoROMS models/SWOR.res1.tc3.lr03.10models.rds")
+blsh_obs=readRDS("/Users/heatherwelch/Dropbox/Eco-ROMS/Model Outputs/Final EcoROMS models/BLSH_obs.res1.tc3.lr03.10models.rds")
+blsh_trk=readRDS("/Users/heatherwelch/Dropbox/Eco-ROMS/Model Outputs/Final EcoROMS models/BLSH_trk.res1.tc3.lr05.10models.noLat.rds")
+casl=readRDS("/Users/heatherwelch/Dropbox/Eco-ROMS/Model Outputs/Final EcoROMS models/casl.res1.tc4.lr.1.10models.rds")
+lbst=readRDS("/Users/heatherwelch/Dropbox/Eco-ROMS/Model Outputs/Final EcoROMS models/LBST.res1.tc3.lr01.10models.rds")
+
+## 1. read in species data
+species=read.csv("/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/observer_casl_swor_blsh_lbst_roms.csv") %>% mutate(SpCd=as.factor(SpCd))
+
+pred_fcn <- function(dataInput,model_object){
+  ## Predict on model data using the best tree for predicting
+  mod_pred10 <- lapply(model_object,FUN=predict.gbm,newdata=dataInput,n.trees=1000,type='response')
+  mod_pred10s <- do.call(cbind,lapply(mod_pred10,data.frame,stringsAsFactors=FALSE))
+  colnames(mod_pred10s) <- as.character(seq(1,10,by=1))
+  meanPred <- apply(mod_pred10s,1,mean)
+  return(meanPred)
+}
+
+species$swor=pred_fcn(species,swor)
+species$blsh_obs=pred_fcn(species,blsh_obs)
+species$blsh_trk=pred_fcn(species,blsh_trk)
+species$casl=pred_fcn(species,casl)
+species$lbst=pred_fcn(species,lbst)
+
+
+### 4. create Eco products under conditions of different species weightings ####
+weightings<-c(-0.1,-0.1,-0.05,-0.9,0.9)
+namesrisk<-c("Blue shark bycatch","Blue sharks","Sea lions","Leatherbacks","Swordfish")
+
+eco_product=function(weightings,dataInput){
+  colnam=paste0("eco_",paste0(weightings,collapse="_"))
+  dataInput=mutate(dataInput,test=blsh_obs*risk[1]+blsh_trk*risk[2]+casl*risk[3]+lbst*risk[4]+swor*risk[5])
+  if(weightings[5]==0){dataInput$test=alt_Rescale(dataInput$test)}
+  else(dataInput$test=Rescale(dataInput$test))
+  names(dataInput)[names(dataInput)=="test"]<-colnam
+  return(dataInput)
+}
+weightings<-c(-0.1,-0.1,-0.05,-0.9,0.9)
+species=eco_product(weightings = weightings,dataInput = species)
+
+weightings<-c(-0.1,-0.1,-0.05,-0.9,0)
+species=eco_product(weightings = weightings,dataInput = species)
--- species_data_prep/Sp_data_prep.R
+++ species_data_prep/Sp_data_prep.R
@@ -10,26 +10,56 @@
 ## "Catch_Spp_1990-2017.csv" = blsh and swor (not sealions not turtles)
 ## "SpList.csv" = species codes (why are some of these numbers?)
 ## "Catch_SeaTurtle_1990-2017.csv" = turtles
-## species codes: casl: UO ;lbst: DC; swordfish and blsh, extract by name
+## species codes: casl: UO ;lbst: DC; swordfish: 91; and blsh: 167, extract by name
+
+library(tidyverse)
+library(maps)
+library(mapdata)
+library(maptools)
+library(rgdal)
+library(sp)
 
 ## extracted observer species data
-roms=read_rds("DGN_allSets_1990-2017_ROMSExtracted.rds")
+roms=readRDS("~/Dropbox/Eco-ROMS/Species Data/Observer Data/DGN_allSets_1990-2017_ROMSExtracted.rds") %>% mutate(TripNumber_Set=as.character(TripNumber_Set))
+set=read.csv("/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/Set_ForHeather.csv") %>% mutate(TripNumber_Set=paste0(TripNumber,"_",Set))
 
 ##----->end output, csv of swor blsh casl lbst tripNumber_sets w associated ROMS data
 
-library(tidyverse)
-
 ### 1. locate raw species data
-casl=read.csv("Catch_Life_1990-2017.csv") %>% filter(SpCd=="UO")%>% mutate(SpCd=as.character(SpCd))
-swor=read.csv("Catch_Spp_1990-2017.csv") %>% filter(ScientificName=="Xiphias gladius") %>% select(TripNumber_Set,SpCd,TotCat) %>% mutate(SpCd=as.character(SpCd))
-blsh=read.csv("Catch_Spp_1990-2017.csv") %>% filter(ScientificName=="Prionace glauca") %>% select(TripNumber_Set,SpCd,TotCat)%>% mutate(SpCd=as.character(SpCd))
-lbst=read.csv("Catch_SeaTurtle_1990-2017.csv") %>% filter(SpCd=="DC") %>% mutate(TripNumber_Set=paste0(TripNumber_Set))  %>% select(TripNumber_Set,SpCd) %>% mutate(TotCat=1)%>% mutate(SpCd=as.character(SpCd))
+casl=read.csv("~/Dropbox/Eco-ROMS/Species Data/Observer Data/Catch_Life_1990-2017.csv") %>% filter(SpCd=="UO")%>% mutate(SpCd=as.character(SpCd)) %>% filter(TripNumber_Set!='')%>% mutate(TripNumber_Set=as.character(TripNumber_Set))
+swor=read.csv("~/Dropbox/Eco-ROMS/Species Data/Observer Data/Catch_Spp_1990-2017.csv") %>% filter(ScientificName=="Xiphias gladius") %>% select(TripNumber_Set,SpCd,TotCat) %>% mutate(SpCd=as.character(SpCd))
+blsh=read.csv("~/Dropbox/Eco-ROMS/Species Data/Observer Data/Catch_Spp_1990-2017.csv") %>% filter(ScientificName=="Prionace glauca") %>% select(TripNumber_Set,SpCd,TotCat)%>% mutate(SpCd=as.character(SpCd))
+lbst=read.csv("~/Dropbox/Eco-ROMS/Species Data/Observer Data/Catch_SeaTurtle_1990-2017.csv") %>% filter(SpCd=="DC") %>% mutate(TripNumber_Set=paste0(TripNumber_Set))  %>% select(TripNumber_Set,SpCd) %>% mutate(TotCat=1)%>% mutate(SpCd=as.character(SpCd))
+
+### cleaning up casl
+cas=inner_join(casl,set,by="TripNumber_Set") %>% mutate(dt=as.Date(paste(Year,MM,DD,sep="-")) )%>% mutate(lat=LatD1+LatM1/60)%>% mutate(lon=LongD1+LongM1/60) %>% select(TripNumber_Set,SpCd,TotCat,dt,lat,lon)
+cas$lon=cas$lon*-1
+write.csv(cas,"/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/cas.csv")
+
+### check Code_LocallyExtract&CalculateVariables.R for how cas_roms.csv was created
+casl_roms=read.csv("/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/cas_roms.csv") 
+casl_roms=casl_roms[,c(3,4,5,6,7,8,9,10,12,13,14,15,16:22,24,25)]
+casl_roms=casl_roms[,c(1:7,9:21,8)]
+
+
+# map("usa", fill=TRUE, col="white", bg="lightblue", ylim=c(-60, 90), mar=c(0,0,0,0))
+# staticdir="~/Dropbox/Eco-ROMS/ROMS & Bathym Data/Bathymetry ETOPO1/"
+# studyarea=readShapeSpatial(paste0(staticdir,"sa_square_coast3.shp"))
+# spdf=SpatialPointsDataFrame(coords=cas[,5:6],data=cas)
+# sp::coordinates(cas)=~lon+lat
+# crs(cas)="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# plot(cas)
+# plot(studyarea,add=T)
+# map('worldHires',add=TRUE,col=grey(0.7),fill=TRUE)
 
 species=do.call("rbind",list(casl,swor,blsh,lbst)) %>% as.tibble() ### final species data frame, raw
-write.csv(species,"/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/observer_casl_swor_blsh_lbst_raw.csv")
+#write.csv(species,"/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/observer_casl_swor_blsh_lbst_raw.csv")
 
 ### 2. associate EcoCast and EcoROMS extractions with species data
 ### ---------------------------------------> EcoROMS
 species_roms=inner_join(species,roms,by="TripNumber_Set")
-write.csv(species,"/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/observer_casl_swor_blsh_lbst_roms.csv")
+species_roms=species_roms[,c(1:6,9,11:21,23:25)]
+species_roms=rbind(casl_roms,species_roms)
+
+write.csv(species_roms,"/Volumes/SeaGate/EcoCast_EcoROMS_comparison_ms/EcoCast_EcoROMS_comparison_ms/raw_data/observer_casl_swor_blsh_lbst_roms.csv")
 
