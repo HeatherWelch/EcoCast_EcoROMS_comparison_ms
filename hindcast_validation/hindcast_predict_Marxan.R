07b1d5141a306c8b591f2a73c911e09aee1204bf
HeatherWelch <heather.welch@noaa.gov>
2018-03-12 10:31:02 -0700

    commits for historical run

--- hindcast_validation/hindcast_predict_Marxan.R
+++ hindcast_validation/hindcast_predict_Marxan.R
@@ -13,32 +13,32 @@ outdir="~/Dropbox/Eco-ROMS/EcoROMSruns/output/marxan/"
 
 
 ##### hindcasting 2005 08-11 run
-dates=seq(as.Date("2005-08-01",format="%Y-%m-%d"),as.Date("2005-11-30",format="%Y-%m-%d"),by="day") %>% as.character()
+# dates=seq(as.Date("2005-08-01",format="%Y-%m-%d"),as.Date("2005-11-30",format="%Y-%m-%d"),by="day") %>% as.character()
+# 
+# weightings<-c(-0.1,-0.1,-0.05,-0.2,0.1) ## (run 1)
+# for(d in dates){
+#   print(d)
+#   get_date=d
+#   scp(get_date = get_date,biofeats = biofeats,cost=cost,dailypreddir = dailypreddir,weightings = weightings,namesrisk = namesrisk)
+# }
+# 
+# weightings<-c(-0.1,-0.1,-0.05,-0.3,0.6) # testing swor at it's most extreme ## (run 2)
+# for(d in dates){
+#   print(d)
+#   get_date=d
+#   scp(get_date = get_date,biofeats = biofeats,cost=cost,dailypreddir = dailypreddir,weightings = weightings,namesrisk = namesrisk)
+# }
+# 
+# 
+# weightings<-c(-0.1,-0.1,-0.05,-0.3,0.1) # testing leatherback at it's most extreme, swor neutral ## (run 3)
+# for(d in dates){
+#   print(d)
+#   get_date=d
+#   scp(get_date = get_date,biofeats = biofeats,cost=cost,dailypreddir = dailypreddir,weightings = weightings,namesrisk = namesrisk)
+# }
 
-weightings<-c(-0.1,-0.1,-0.05,-0.2,0.1) ## (run 1)
-for(d in dates){
-  print(d)
-  get_date=d
-  scp(get_date = get_date,biofeats = biofeats,cost=cost,dailypreddir = dailypreddir,weightings = weightings,namesrisk = namesrisk)
-}
 
-weightings<-c(-0.1,-0.1,-0.05,-0.3,0.6) # testing swor at it's most extreme ## (run 2)
-for(d in dates){
-  print(d)
-  get_date=d
-  scp(get_date = get_date,biofeats = biofeats,cost=cost,dailypreddir = dailypreddir,weightings = weightings,namesrisk = namesrisk)
-}
-
-
-weightings<-c(-0.1,-0.1,-0.05,-0.3,0.1) # testing leatherback at it's most extreme, swor neutral ## (run 3)
-for(d in dates){
-  print(d)
-  get_date=d
-  scp(get_date = get_date,biofeats = biofeats,cost=cost,dailypreddir = dailypreddir,weightings = weightings,namesrisk = namesrisk)
-}
-
-
-##### hindcasting 1997 10-11 run
+##### hindcasting 1997 10-11 run  -----> running
 dates=seq(from=as.Date("1997-10-01",format="%Y-%m-%d"),to=as.Date("1997-11-30",format="%Y-%m-%d"),by="day") %>% as.character()
 
 weightings<-c(-0.1,-0.1,-0.05,-0.2,0.1) ## (run 1)
