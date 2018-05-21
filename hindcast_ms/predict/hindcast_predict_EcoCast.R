32314d6be602d709b48d9df4a91788906b70fadc
HeatherWelch <heather.welch@noaa.gov>
2018-05-21 15:39:10 -0700

    run B

--- hindcast_ms/predict/hindcast_predict_EcoCast.R
+++ hindcast_ms/predict/hindcast_predict_EcoCast.R
@@ -14,7 +14,7 @@ weightings<-c(-0.1,-0.1,-0.05,-0.4,0.2)
 dates=list.files("~/Dropbox/Eco-ROMS/EcoROMSruns/output/hindcast/marxan",pattern=".grd") %>% grep("_raw",.,value=T) %>% grep(paste0(weightings,collapse="_"),.,value=T) %>% 
   gsub(paste0("marxan_nocost_",paste0(weightings,collapse="_"),"_"),"",.) %>% gsub("_raw.grd","",.)
 
-## scenario A--> isolating the effect of swordfish weightings (5 runs, weightings are the same for both algorithms) (running) #### 
+## scenario A--> isolating the effect of swordfish weightings (5 runs, weightings are the same for both algorithms) (run) #### 
 weightings <-c(0,0,0,0,0.1) #run A.1
 for(d in dates){
   get_date=d
@@ -51,4 +51,38 @@ for(d in dates){
 }
 
 
+## scenario B--> isolating the effect of leatherback weightings (5 runs, weightings are the same for both algorithms) (running) ####
+weightings <-c(0,0,0,-0.1,0) #run B.1
+for(d in dates){
+  get_date=d
+  print(get_date)
+  Run_ecoroms_hindcast(get_date=get_date,moddir=moddir,dailypreddir = dailypreddir,outdir = outdir,EcoROMSdir = EcoROMSdir,namesrisk=namesrisk,weightings=weightings,studyarea=studyarea,staticdir=staticdir)
+}
 
+weightings <-c(0,0,0,-0.3,0) #run B.2
+for(d in dates){
+  get_date=d
+  print(get_date)
+  Run_ecoroms_hindcast(get_date=get_date,moddir=moddir,dailypreddir = dailypreddir,outdir = outdir,EcoROMSdir = EcoROMSdir,namesrisk=namesrisk,weightings=weightings,studyarea=studyarea,staticdir=staticdir)
+}
+
+weightings <-c(0,0,0,-0.5,0) #run B.3
+for(d in dates){
+  get_date=d
+  print(get_date)
+  Run_ecoroms_hindcast(get_date=get_date,moddir=moddir,dailypreddir = dailypreddir,outdir = outdir,EcoROMSdir = EcoROMSdir,namesrisk=namesrisk,weightings=weightings,studyarea=studyarea,staticdir=staticdir)
+}
+
+weightings <-c(0,0,0,-0.7,0) #run B.4
+for(d in dates){
+  get_date=d
+  print(get_date)
+  Run_ecoroms_hindcast(get_date=get_date,moddir=moddir,dailypreddir = dailypreddir,outdir = outdir,EcoROMSdir = EcoROMSdir,namesrisk=namesrisk,weightings=weightings,studyarea=studyarea,staticdir=staticdir)
+}
+
+weightings <-c(0,0,0,-0.9,0) #run B.5
+for(d in dates){
+  get_date=d
+  print(get_date)
+  Run_ecoroms_hindcast(get_date=get_date,moddir=moddir,dailypreddir = dailypreddir,outdir = outdir,EcoROMSdir = EcoROMSdir,namesrisk=namesrisk,weightings=weightings,studyarea=studyarea,staticdir=staticdir)
+}
